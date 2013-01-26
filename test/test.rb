d9d1bc8d0eec3d28bc6311e57f2f90a22167c614
itsZero (Chien-An Cho) <itszero@gmail.com>
2013-01-26 00:23:43 -0500

    a long overdue code reindentation

--- test/test.rb
+++ test/test.rb
@@ -3,17 +3,17 @@ require '../U420'
 require 'serialport'
 
 serial_out = SerialPort.new("/dev/tty.usbserial",
-			:baud_rate => 9600,
-			:databits => 8,
-			:parity => SerialPort::NONE,
-			:stopbits => 1)
+                            :baud_rate => 9600,
+                            :databits => 8,
+                            :parity => SerialPort::NONE,
+                            :stopbits => 1)
 
 prt = InvoicePrinter::U420.new({
-	:header => "InvoicePrinter\nU420\n",
-	:lines_total => 35,
-	:lines_available => 18,
-	:lines_stamp => 5,
-	:output => serial_out
+  :header => "InvoicePrinter\nU420\n",
+  :lines_total => 35,
+  :lines_available => 18,
+  :lines_stamp => 5,
+  :output => serial_out
 })
 
 prt.println "Hello, World."
